0030d49fb4b732e1f26ddc2f42eab236af4d0c59
YiXiaoBuYi <130463042+YiXiaoBuYi@users.noreply.github.com>
2024-09-28 16:22:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '嗨',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ox7Lj6iuIgFlW6y_b99MFo8o1lG8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ox7Lj6iuIgFlW6y_b99MFo8o1lG8',
+      useTemplateId: 'D3NopxOKk94QCBi8XNoONg7kHmAmxxT87ij6OcZlJ3c',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-11',
       festivals: [
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '小新',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ox7Lj6iuIgFlW6y_b99MFo8o1lG8',
     }
   ],
 
